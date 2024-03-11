241a524860c705764f83bea198c30a88beb50c08
SHIFU <tamangnischal2018@gmail.com>
2024-03-11 13:59:23 +0545

    first

---
+++ src/test/java/com/selflearn/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
