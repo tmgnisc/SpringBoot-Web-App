44a8be75c2ad89b25f16e826020c2f3e9498549e
SHIFU <tamangnischal2018@gmail.com>
2024-03-13 19:01:32 +0545

    controller for createproduct

--- src/main/java/com/selflearn/controllers/ProductController.java
+++ src/main/java/com/selflearn/controllers/ProductController.java
@@ -6,12 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.selflearn.model.Product;
 import com.selflearn.model.ProductDto;
 import com.selflearn.services.ProductsRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/products")
@@ -34,4 +40,19 @@ public class ProductController {
 		model.addAttribute("productDto", productDto);
 		return "products/CreateProduct";
 	}
+	
+	@PostMapping("/create")
+	public String createProduct(
+			@Valid @ModelAttribute ProductDto productDto,
+			BindingResult result
+			) {
+		
+		if (productDto.getImageFile().isEmpty()) {
+			result.addError(new FieldError("productDto", "imageFile", "The image File is required"));
+		}
+		
+		return "redirect:/products";
+	}
+	
+	
 }
